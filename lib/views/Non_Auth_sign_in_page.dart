0c716febe5ba533b0ec1909dddc30f939ae74595
102550264Brathaban <69609273+102550264Brathaban@users.noreply.github.com>
2021-07-25 14:29:44 +1000

    Create Non_Auth_sign_in_page.dart
    Brath_signin_page

---
+++ lib/views/Non_Auth_sign_in_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_builder.dart';
+
+
+
+/// Entrypoint example for sign in via Email/Password.
+class SigninPage extends StatefulWidget {
+  /// The page title.
+  final String title = 'Sign_in';
+
+  @override
+  State<StatefulWidget> createState() => _signinPageState();
+}
+
+class _signinPageState extends State<SigninPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _success;
+  String _userEmail = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+          key: _formKey,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    alignment: Alignment.center,
+                    child: SignInButtonBuilder(
+                      icon: Icons.person_add,
+                      backgroundColor: Colors.blueGrey,
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          await _signin();
+                        }
+                      },
+                      text: 'signin',
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(_success == null
+                        ? ''
+                        : (_success
+                            ? 'Successfully signed in $_userEmail'
+                            : 'sign in failed')),
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // Example code for sign in logic
+  Future<void> _signin() async {
+
+   // logic to access database and validate username password
+   // access controller to validate???
+   //create a sign in controller???
+  
+  }
+}
