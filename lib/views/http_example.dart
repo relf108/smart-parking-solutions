e05bfca1c2b505f812eafc8333034308f30783ea
GeeKayK <80227386+GeeKayK@users.noreply.github.com>
2021-08-11 16:53:15 +1000

    Update http_example.dart

--- lib/views/http_example.dart
+++ lib/views/http_example.dart
@@ -5,6 +5,21 @@ import 'package:smart_parking_solutions/views/search_spaces.dart';
 import 'package:smart_parking_solutions/views/create_password_view.dart';
 import 'package:smart_parking_solutions/widgets/json_grid.dart';
 import 'package:smart_parking_solutions_common/smart_parking_solutions_common.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+const urlPrefix = 'http://10.0.2.2:5000';
+String responsestring = '';
+
+Future<String> makeGetRequest(String user) async {
+  final url = Uri.parse('http://10.0.2.2:5000/currentBookings?user=' + user);
+  Response response = await get(url);
+  print('Status code: ${response.statusCode}');
+  print('Headers: ${response.headers}');
+  print('Body: ${response.body}');
+  responsestring = await response.body;
+  return responsestring;
+}
 
 ///EXAMPLE STATELESS WIDGET
 class HttpExample extends StatefulWidget {
@@ -15,6 +30,7 @@ class HttpExample extends StatefulWidget {
 /// This is the private State class that goes with MyStatefulWidget.
 class _HttpExample extends State<HttpExample> {
   late TextEditingController _controller;
+  final _formKey = GlobalKey<FormState>();
   String _results = '';
   @override
   void initState() {
@@ -31,23 +47,65 @@ class _HttpExample extends State<HttpExample> {
   @override
   Widget build(BuildContext context) {
     final ratioGen = new ScreenRatioGenerator(context: context);
+    final username = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: ratioGen.screenHeightPercent(percent: 6),
         title: const Text('HTTP GET EXAMPLE'),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-                decoration: const InputDecoration(
-              icon: Icon(Icons.person),
-              hintText: 'Username',
-              labelText: 'Test#',
-            ))
-          ],
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+            Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: ratioGen.screenWidthPercent(percent: 50),
+                      child: TextFormField(
+                        controller: username,
+                        decoration: InputDecoration(
+                            icon: Icon(Icons.person), labelText: 'Username:'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                        onPressed: () => [
+                          makeGetRequest(username.text),
+                          showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title:
+                                  Text('User ' + username.text + ' response:'),
+                              content: Text(responsestring),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.pop(context, 'Cancel'),
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context, 'OK'),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                        child: const Text('Submit'),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ]),
       ),
     );
   }
