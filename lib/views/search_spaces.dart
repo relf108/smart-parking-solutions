bed9f5c6da72ef7969463c928facab508ae41bf2
yuvi4444 <ralhyuvi4@gmail.com>
2021-08-14 22:31:35 +1000

    Update search_spaces.dart

--- lib/views/search_spaces.dart
+++ lib/views/search_spaces.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:dimension_ratios/screen_ratio_generator.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_parking_solutions/views/sign_in_view.dart';
@@ -5,6 +7,7 @@ import 'package:smart_parking_solutions/views/search_spaces.dart';
 import 'package:smart_parking_solutions/views/create_password_view.dart';
 import 'package:smart_parking_solutions/widgets/json_grid.dart';
 import 'package:smart_parking_solutions_common/smart_parking_solutions_common.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class SearchSpacesView extends StatefulWidget {
   @override
@@ -13,11 +16,29 @@ class SearchSpacesView extends StatefulWidget {
 
 /// This is the private State class that goes with MyStatefulWidget.
 class _SearchSpacesView extends State<SearchSpacesView> {
+  late int _radius = 100;
+  var _value;
+  double _duration = 0.5;
+  String _date = "Not set";
+  String _time = "Not set";
   late TextEditingController _controller;
   String _results = '';
   @override
   void initState() {
     super.initState();
+  }
+
+  void reset() {
+    //textformfield.clear();
+
+  Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => super.widget));
+
+    setState(() {
+      _value = null;
+    });
   }
 
   @override
@@ -35,18 +56,209 @@ class _SearchSpacesView extends State<SearchSpacesView> {
           title: const Text('Search Parking Spaces'),
         ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: ratioGen.screenWidthPercent(percent: 100),
-                child: TextFormField(
-                  decoration:
-                      InputDecoration(labelText: 'Where are you going?'),
+          child:  Column(
+            children: <Widget> [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: ratioGen.screenWidthPercent(percent: 90),
+                    child: TextFormField(
+                      decoration:
+                          InputDecoration(labelText: 'Enter destination here:'),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                  children: <Widget>[
+                      Container(
+                      width: ratioGen.screenWidthPercent(percent: 90),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          DatePicker.showDatePicker(context,
+                          showTitleActions: true,
+                          minTime: DateTime(2000, 1, 1),
+                          maxTime: DateTime(2022, 12, 31), onConfirm: (date) {
+                            print('confirm $date');
+                            _date = '${date.year} - ${date.month} - ${date.day}';
+                            setState(() {});
+                          }, currentTime: DateTime.now(), locale: LocaleType.en);
+                        },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.date_range,
+                                  size: 18.0,
+                                ),
+                                Text(" $_date"),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Text("Select date"),
+                    ],
+                  ),
+                ),
+              ),
+                    ),
+                  ]
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                  children: <Widget>[
+                      Container(
+                      width: ratioGen.screenWidthPercent(percent: 90),
+                      child: ElevatedButton(
+                        onPressed: () {
+                        DatePicker.showTimePicker(context,
+                        showTitleActions: true, onConfirm: (time) {
+                          print('confirm $time');
+                          _time = '${time.hour} : ${time.minute}';
+                          setState(() {});
+                        },
+                        currentTime: DateTime.now(), locale: LocaleType.en);
+                        setState(() {});
+                        },
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                            children: <Widget>[
+                            Container(
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.access_time,
+                                  size: 18.0,
+                                ),
+                                Text(" $_time"),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Text("  Select time"),
+                    ],
+                  ),
                 ),
               )
-            ],
-          ),
-        ));
+                    ),
+                  ]
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                  children: <Widget>[
+                      Container(
+                      width: ratioGen.screenWidthPercent(percent: 90),
+                      child: Column(
+                        children: <Widget> [
+                          DropdownButtonFormField<double>(
+                            value: _duration, 
+                            items: [0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0]
+                                .map((label) => DropdownMenuItem(
+                                  child: Text(label.toString()),
+                                  value: label,
+                                  )).toList(),
+                                  decoration: InputDecoration(labelText: 'Parking duration (hours)'),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _duration = value!;
+                                    }
+                                    );
+                                  }
+                            )
+                        ]
+                      )
+                    ),
+                  ]
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                  children: <Widget>[
+                      Container(
+                      width: ratioGen.screenWidthPercent(percent: 90),
+                      child: Column(
+                        children: <Widget> [
+                          DropdownButtonFormField<int>(
+                            value: _radius, 
+                            items: [100,200,500,1000,2000]
+                                .map((label) => DropdownMenuItem(
+                                  child: Text(label.toString()),
+                                  value: label,
+                                  )).toList(),
+                                  hint: Text('Radius'),
+                                  decoration: InputDecoration(labelText: 'Radius (m)'),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _radius = value!;
+                                    }
+                                    );
+                                  }
+                            )
+                        ]
+                      )
+                    ),
+                  ]
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: ratioGen.screenHeightPercent(percent: 5),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                      Container(
+                      width: ratioGen.screenWidthPercent(percent: 30),
+                      child: ElevatedButton(
+                            onPressed: () => {
+                              reset()
+                            },   
+                            child: Text('Reset')         
+                      ),
+                    ),
+                      Container(
+                        width: ratioGen.screenWidthPercent(percent: 30),
+                        child: ElevatedButton(
+                            onPressed: () => {},   
+                            child: Text('Submit')         
+                      ),
+                     ),
+                ]
+                  ),
+                ],
+              ),
+          )
+        );
   }
 }
