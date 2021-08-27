9f0e5e0fd5e4260d4a4404097bfaad467a72e00c
102550264Brathaban <69609273+102550264Brathaban@users.noreply.github.com>
2021-08-27 21:58:14 +1000

    Update reserve_space.dart

--- lib/views/reserve_space.dart
+++ lib/views/reserve_space.dart
@@ -136,25 +136,155 @@ class _ReserveSpaceView extends State<ReserveSpaceView> {
     );
   }
 
+  // Widget getListView() {
+  //   final DateTime bookingdate = widget.bookingdate;
+  //   final String duration = widget.duration;
+  //   var listView = ListView.builder(
+  //       itemCount: _bays.length,
+  //       itemBuilder: (context, index) {
+
   Widget getListView() {
     final DateTime bookingdate = widget.bookingdate;
     final String duration = widget.duration;
-    var listView = ListView.builder(
-        itemCount: _bays.length,
-        itemBuilder: (context, index) {
-          // INSERT YOUR CODE HERE
-          // VARIABLES/FUNCTIONS TO BE USED:
-          // _bays : list of all bays ( e.g. _bays[i]['streetMarkerID'])
-          // bookingdate : the booking date and time (e.g. 2021-08-31 03:39:05.92) that will need to be passed into getReserveSpace()
-          // duration : the length of booking time (e.g. 1.5 hours) that will need to be passed into getReserveSpace()
-          // getReserveSpace() : does the http request and validation, just set the booking button onpress/onclick/ontap to action this function
-          // loading : this variable returns the loading animation alert dialog, wrap this in a 'showDialog' to use it.
+    // var listView = ListView.builder(
+    //     itemCount: _bays.length,
+    //     itemBuilder: (context, index) {
 
-          // In this section you will have to program your version on displaying the list items,
-          // you do not need to touch any other code in the file
-          // Only work on lines 133 and on
-          throw 'test';
-        });
-    return listView;
+    var gView = GridView.builder(
+        itemCount: _bays.length, // The length Of the array
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.6,
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+        ), // The size of the grid box
+        itemBuilder: (context, index) => Container(
+                child: Card(
+              color: Colors.green,
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      _bays[index]["streetMarkerID"].toString(),
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                    Container(height: 34.0),
+                    Text(
+                      'Location',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.white,
+                          backgroundColor: Colors.brown,
+                          fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                    Container(height: 7.0),
+                    Text(_bays[index]["lat"] + ', ' + _bays[index]["long"]),
+                    Container(height: 50.0),
+                    Text('Parking Restriction',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.white,
+                            backgroundColor: Colors.brown,
+                            fontSize: 16)),
+                    Container(height: 7.0),
+                    Text((() {
+                      if (_bays[index]["description"].length == 1) {
+                        return _bays[index]["description"]["description1"];
+                      } else if (_bays[index]["description"].length == 2) {
+                        return _bays[index]["description"]["description1"] +
+                            ', ' +
+                            _bays[index]["description"]["description2"];
+                      } else if (_bays[index]["description"].length == 3) {
+                        return _bays[index]["description"]["description1"] +
+                            ', ' +
+                            _bays[index]["description"]["description2"] +
+                            ', ' +
+                            _bays[index]["description"]["description3"];
+                      } else if (_bays[index]["description"].length == 4) {
+                        return _bays[index]["description"]["description1"] +
+                            ', ' +
+                            _bays[index]["description"]["description2"] +
+                            ', ' +
+                            _bays[index]["description"]["description3"] +
+                            ', ' +
+                            _bays[index]["description"]["description4"];
+                      } else if (_bays[index]["description"].length == 5) {
+                        return _bays[index]["description"]["description1"] +
+                            ', ' +
+                            _bays[index]["description"]["description2"] +
+                            ', ' +
+                            _bays[index]["description"]["description3"] +
+                            ', ' +
+                            _bays[index]["description"]["description4"] +
+                            ', ' +
+                            _bays[index]["description"]["description5"];
+                      } else {
+                        return 'Error reading restrictions';
+                      }
+                    })()),
+                    Container(height: 16.0),
+                    InkWell(
+                      onTap: () async {
+                        showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const Text('Confirm Booking'),
+                            content: Text(
+                                'Are you sure that you would like to book Parking Bay ' +
+                                    _bays[index]["streetMarkerID"].toString() +
+                                    ' at your chosen time?'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.pop(context, 'Cancel'),
+                                child: const Text('No'),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  Navigator.pop(context);
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return loading;
+                                    },
+                                  );
+                                  getReserveSpace(
+                                      int.parse(_bays[index]["bayID"]),
+                                      bookingdate,
+                                      duration,
+                                      testuser,
+                                      _bays[index]['streetMarkerID'],
+                                      _bays[index]['lat'],
+                                      _bays[index]['long']);
+                                },
+                                child: const Text('Yes'),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      child: Icon(Icons.book_online),
+                    ),
+                  ],
+                ),
+              ),
+            ))
+        //return gView;
+
+        // INSERT YOUR CODE HERE
+        // VARIABLES/FUNCTIONS TO BE USED:
+        // _bays : list of all bays ( e.g. _bays[i]['streetMarkerID'])
+        // bookingdate : the booking date and time (e.g. 2021-08-31 03:39:05.92) that will need to be passed into getReserveSpace()
+        // duration : the length of booking time (e.g. 1.5 hours) that will need to be passed into getReserveSpace()
+        // getReserveSpace() : does the http request and validation, just set the booking button onpress/onclick/ontap to action this function
+        // loading : this variable returns the loading animation alert dialog, wrap this in a 'showDialog' to use it.
+
+        // In this section you will have to program your version on displaying the list items,
+        // you do not need to touch any other code in the file
+        // Only work on lines 133 and on
+        // throw 'test';
+        );
+    return gView;
   }
 }
